abc0fbdac01fe1a9d7da7056d98917e9c26b64ea
Adir Shaban <adirshaban1995@gmail.com>
2018-09-23 18:55:11 +0300

    changed the completeauth handler, add handler for get AuthURL

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"github.com/zmb3/spotify"
 )
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
 
 const (
 	redirectURI = "http://localhost:8080/callback"
@@ -31,55 +27,58 @@ func RandStringBytesRmndr(n int) string {
 
 var (
 	auth          = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
-	ch            = make(chan *spotify.Client)
-	state         = RandStringBytesRmndr(10)
 	spotifyClient *spotify.Client
+	redisClient   *redis.Client
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/callback", completeAuth)
-	go r.Run(":8080")
-	http.HandleFunc("/search", searchSpotify)
-	http.HandleFunc("/AuthURL", getAuthURL)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got request for:", r.URL.String())
-	})
-
-	// go http.ListenAndServe(":8080", nil)
-
-	url := auth.AuthURL(state)
-	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
-	spotifyClient = <-ch
-
-	user, err := spotifyClient.CurrentUser()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("You are logged in as:", user.ID)
-
+	r.GET("/authurl", getAuthURL)
+	r.Run(":8080")
 }
 
 func completeAuth(c *gin.Context) {
-	tok, err := auth.Token(state, c.Request)
+	queryState := c.Query("state")
+	tok, err := auth.Token(queryState, c.Request)
 	if err != nil {
 		log.Fatal(err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 	}
+	state, err := redisClient.Get(queryState).Result()
+	if err != nil {
+		panic(err)
+	}
 
 	// check if the current request has the same context
-	if st := c.Query("state"); st != state {
+	if state == "" {
 		c.JSON(http.StatusNotFound, gin.H{"error": "State mismatch"})
-		log.Fatalf("State mismatch: %s != %s\n", st, state)
+		log.Fatalf("State does not exist: %s\n", queryState)
 	}
-	client := auth.NewClient(tok)
-	c.String(http.StatusOK, "Login Complete")
-	ch <- &client
+
+	c.JSON(http.StatusOK, gin.H{"accessToken": tok.AccessToken})
 }
 
-func getAuthURL(w http.ResponseWriter, r *http.Request) {
+func getAuthURL(c *gin.Context) {
+	state := RandStringBytesRmndr(10)
+	url := auth.AuthURL(state)
 
+	err := redisClient.Set(state, 0, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"authurl": url})
 }
 
 func searchSpotify(w http.ResponseWriter, r *http.Request) {
