2a2895200c5194b830bd4fd197e59133c60265be
Adir Shaban <adirshaban1995@gmail.com>
2018-10-14 14:14:49 +0300

    changed the login button, now have the token yay

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -3,9 +3,32 @@ import LoginButton from '../LoginButton';
 import './index.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    const params = this.getHashParams();
+    this.state = {
+      accessToken: params.access_token
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
   render() {
     return (
       <div className="app-container">
+        <div className="logo">
+          <img src="public/logo.jpeg" alt="Logo" />
+        </div>
         <LoginButton />
       </div>
     );
--- client/src/components/LoginButton/index.js
+++ client/src/components/LoginButton/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import {Button} from 'antd';
 
 export default () => (
-    <Button style={{backgroundColor: 'green'}} type="primary">Login to Spotify</Button>
+    <a href="http://localhost:8080/login" className="button" style={{ 
+        zIndex: 2,
+        backgroundColor: "#8dea8d",
+        margin: "0 auto",
+        display: "block"
+    }}>Login to Spotify</a>
 );
